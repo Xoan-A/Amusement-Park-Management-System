3f11b31c9599950b82602c5a59564de77e40d09d
Xoan Atrio <atrioxoan@gmail.com>
2025-09-28 12:04:45 -0300

    test: [RED] add tests for AttractionRepository

---
+++ obligatorio_parque_tematico_virtual/TestDataAccess/AttractionRepositoryTest.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using DataAccess.Context;
+using DataAccess.Repositories;
+using IDataAccess;
+
+namespace TestDataAccess;
+
+[TestClass]
+public class AttractionRepositoryTest
+{
+    private AppDbContext _context;
+    private IAttractionRepository _attractionRepository;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        DbContextOptions<AppDbContext> options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        _context = new AppDbContext(options);
+        _attractionRepository = new AttractionRepository(_context);
+        
+        Attraction attraction = new Attraction();
+        
+        attraction.Name = "Race simulator";
+        attraction.Description = "average race simulator";
+        attraction.Type = AttractionType.Simulator;
+        attraction.MinAge = 18;
+        attraction.MaxCapacity = 10;
+        attraction.CurrentCapacity = 0;
+        attraction.IsActive = true;
+    }
+    
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Dispose();
+    }
+    
+    [TestMethod]
+        public void Create_ShouldAddAttractionToDatabase()
+        {
+            Attraction result = _attractionRepository.Create(attraction);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Race simulator", result.Name);
+            Assert.AreEqual(1, _context.Attractions.Count());
+        }
+
+        [TestMethod]
+        public void GetByName_ShouldReturnAttraction_WhenAttractionExists()
+        {
+            _context.Attractions.Add(attraction);
+            _context.SaveChanges();
+
+            User result = _attractionRepository.GetByName("Race simulator");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Race simulator", result.Name);
+        }
+
+        [TestMethod]
+        public void GetByName_ShouldReturnNull_WhenAttractionDoesNotExist()
+        {
+            User result = _attractionRepository.GetByName("nonexistent");
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetById_ShouldReturnAttraction_WhenAttractionExists()
+        {
+            _context.Attractions.Add(attraction);
+            _context.SaveChanges();
+
+            User result = _attractionRepository.GetById(attraction.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(attraction.Id, result.Id);
+        }
+
+        [TestMethod]
+        public void GetById_ShouldReturnNull_WhenAttractionDoesNotExist()
+        {
+            Guid nonExistentId = Guid.NewGuid();
+
+            User result = _attractionRepository.GetById(nonExistentId);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void IsNameUnique_ShouldReturnFalse_WhenNameExists()
+        {
+            _context.Attractions.Add(attraction);
+            _context.SaveChanges();
+
+            bool result = _attractionRepository.IsNameUnique("Race simulator");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsNameUnique_ShouldReturnTrue_WhenNameDoesNotExist()
+        {
+            bool result = _attractionRepository.IsNameUnique("new name");
+
+            Assert.IsTrue(result);
+        }
+}
