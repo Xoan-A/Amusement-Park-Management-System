183ec3e241c4d284d712c2e56c7706b3210e347e
Xoan Atrio <atrioxoan@gmail.com>
2025-09-28 13:42:40 -0300

    test [RED] add test setup for AttractionService

---
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/AttractionServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using Domain;
+using IBusinessLogic;
+using IDataAccess;
+using BusinessLogic;
+
+namespace TestBusinessLogic;
+
+[TestClass]
+public class AttractionServiceTest
+{
+    private Mock<IAttractionRepository> _mockAttractionRepository;
+    private IAttractionService _attractionService;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockAttractionRepository = new Mock<IAttractionRepository>();
+        _attractionService = new AttractionService(_mockAttractionRepository.Object, _mockAttractionRepository.Object);
+    }
+}
