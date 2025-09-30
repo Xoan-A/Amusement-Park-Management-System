477b50ac720cc70fd31a27d9ec2bf027c633d509
Xoan Atrio <atrioxoan@gmail.com>
2025-09-30 16:22:44 -0300

    test: [RED] EventService GetAllEvents_ShouldReturnListOfEvents test created

--- obligatorio_parque_tematico_virtual/TestBusinessLogic/EventServiceTest.cs
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/EventServiceTest.cs
@@ -41,4 +41,76 @@ public class EventServiceTest
         Assert.AreEqual(expectedEvent.Name, result.Name);
         _mockEventRepository.Verify(r => r.GetById(expectedEvent.Id), Times.Once);
     }
-}
+    
+    [TestMethod]
+    public async Task GetEventById_ShouldReturnEventWithAttractions_WhenEventHasAttractions()
+    {
+        Attraction attraction1 = new Attraction
+        {
+            Id = Guid.NewGuid(),
+            Name = "Roller Coaster",
+            Description = "A thrilling ride",
+            Type = AttractionType.RollerCoaster,
+            MinAge = 12,
+            MaxCapacity = 20,
+            CurrentCapacity = 5,
+            IsActive = true
+        };
+        
+        Event expectedEvent = new Event
+        {
+            Name = "Fun Fair",
+            Date = new DateTime(2024, 9, 10),
+            Hour = 14,
+            MaxCapacity = 3000,
+            CurrentCapacity = 0,
+            Cost = 50,
+        };
+        
+        expectedEvent.AddAttraction(attraction1);
+        
+        _mockEventRepository.Setup(r => r.GetById(expectedEvent.Id)).ReturnsAsync(expectedEvent);
+        EventResponse result = await _eventService.GetEventById(expectedEvent.Id);
+        
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedEvent.Name, result.Name);
+        Assert.AreEqual(1, result.Attractions.Count);
+        Assert.IsTrue(result.Attractions.Any(a => a.Name == "Roller Coaster"));
+        
+        _mockEventRepository.Verify(r => r.GetById(expectedEvent.Id), Times.Once);
+    }
+    
+    [TestMethod]
+    public async Task GetAllEvents_ShouldReturnListOfEvents()
+    {
+        List<Event> expectedEvents = new List<Event>
+        {
+            new Event
+            {
+                Name = "Food Festival",
+                Date = new DateTime(2024, 7, 20),
+                Hour = 12,
+                MaxCapacity = 2000,
+                CurrentCapacity = 0,
+                Cost = 30,
+                Attractions = []
+            },
+            new Event
+            {
+                Name = "Art Expo",
+                Date = new DateTime(2024, 10, 5),
+                Hour = 9,
+                MaxCapacity = 1500,
+                CurrentCapacity = 0,
+                Cost = 20,
+                Attractions = []
+            }
+        };
+        _mockEventRepository.Setup(r => r.GetAll()).ReturnsAsync(expectedEvents);
+        List<EventResponse> result = await _eventService.GetAllEvents();
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual("Food Festival", result[0].Name);
+        Assert.AreEqual("Art Expo", result[1].Name);
+        _mockEventRepository.Verify(r => r.GetAll(), Times.Once);
+    }
