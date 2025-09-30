5bf7528f01fca89c348f61ea20407e21abfdb6e8
Xoan Atrio <atrioxoan@gmail.com>
2025-09-30 12:57:30 -0300

    feat: [GREEN] Event class created with basic properties

---
+++ obligatorio_parque_tematico_virtual/Domain/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public int Hour { get; set; }
+    public int MaxCapacity { get; set; }
+    public int CurrentCapacity { get; set; }
+    public decimal Cost { get; set; }
+    public List<Attraction> Attractions { get; set; }
+}
--- obligatorio_parque_tematico_virtual/TestDomain/EventTest.cs
+++ obligatorio_parque_tematico_virtual/TestDomain/EventTest.cs
@@ -9,24 +9,25 @@ public class EventTest
     [TestMethod]
     public void Event_ShouldHaveRequiredProperties()
     {
-        Event event = new Event();
-        
-        event.Id = Guid.NewGuid();
-        event.Name = "Music Festival";
-        event.Date = new DateTime(2024, 8, 15);
-        event.Hour = 10;
-        event.MaxCapacity = 5000;
-        event.CurrentCapacity = 0;
-        event.Cost = 100;
-        event.Attractions = new List<Attraction>();
-        
-        Assert.IsNotNull(event.Id);
-        Assert.AreEqual("Music Festival", event.Name);
-        Assert.AreEqual(new DateTime(2024, 8, 15), event.Date);
-        Assert.AreEqual(10, event.Hour);
-        Assert.AreEqual(5000, event.MaxCapacity);
-        Assert.AreEqual(0, event.CurrentCapacity);
-        Assert.AreEqual(100, event.Cost);
-        Assert.IsNotNull(event.Attractions);
+        Event newEvent = new Event
+        {
+            Id = Guid.NewGuid(),
+            Name = "Music Festival",
+            Date = new DateTime(2024, 8, 15),
+            Hour = 10,
+            MaxCapacity = 5000,
+            CurrentCapacity = 0,
+            Cost = 100,
+            Attractions = []
+        };
+
+        Assert.IsNotNull(newEvent.Id);
+        Assert.AreEqual("Music Festival", newEvent.Name);
+        Assert.AreEqual(new DateTime(2024, 8, 15), newEvent.Date);
+        Assert.AreEqual(10, newEvent.Hour);
+        Assert.AreEqual(5000, newEvent.MaxCapacity);
+        Assert.AreEqual(0, newEvent.CurrentCapacity);
+        Assert.AreEqual(100, newEvent.Cost);
+        Assert.IsNotNull(newEvent.Attractions);
     }
 }
