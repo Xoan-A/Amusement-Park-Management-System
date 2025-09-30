f16fc23d971fcc37f0306b385389e2df8f191d20
Xoan Atrio <atrioxoan@gmail.com>
2025-09-30 14:19:14 -0300

    test: [RED] EventService test class created and GetEventById_ShouldReturnEvent_WhenIdIsValid test created

---
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/EventServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Domain;
+using IBusinessLogic;
+using IDataAccess;
+using BusinessLogic;
+using Models.In;
+using Models.Out;
+
+namespace TestBusinessLogic;
+
+[TestClass]
+public class EventServiceTest
+{
+    private Mock<IEventRepository> _mockEventRepository;
+    private IEventService _eventService;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockEventRepository = new Mock<IEventRepository>();
+        _eventService = new EventService(_mockEventRepository.Object);
+    }
+
+    [TestMethod]
+    public async Task GetEventById_ShouldReturnEvent_WhenIdIsValid()
+    {
+        Event expectedEvent = new Event
+        {
+            Name = "Music Festival",
+            Description = "A day of live music",
+            StartDate = DateTime.Now.AddDays(10),
+            EndDate = DateTime.Now.AddDays(11),
+            IsActive = true
+        };
+        _mockEventRepository.Setup(r => r.GetById(expectedEvent.Id)).ReturnsAsync(expectedEvent);
+        EventResponse result = await _eventService.GetEventById(expectedEvent.Id);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expectedEvent.Name, result.Name);
+        _mockEventRepository.Verify(r => r.GetById(expectedEvent.Id), Times.Once);
+    }
+}
