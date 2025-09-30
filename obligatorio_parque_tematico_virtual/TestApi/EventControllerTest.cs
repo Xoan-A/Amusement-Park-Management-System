a24a3416b59fa91b05b821d67a6692f2f65c1ad3
Xoan Atrio <atrioxoan@gmail.com>
2025-09-30 18:07:05 -0300

    test: [RED] EventController test class created with GetEvents_ReturnsOkResult_WithListOfEvents test

---
+++ obligatorio_parque_tematico_virtual/TestApi/EventControllerTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Domain;
+using IBusinessLogic;
+using Models.In;
+using Models.Out;
+
+namespace ApiTests;
+
+[TestClass]
+public class EventControllerTest
+{
+    private WebApplicationFactory<Program> _factory = null!;
+    private HttpClient _client = null!;
+    private HttpClient _adminClient = null!;
+    private Mock<IEventService> _mockEventService = null!;
+    
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockEventService = new Mock<IEventService>();
+
+        _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services => { services.AddSingleton(_mockEventService.Object); });
+        });
+
+        _client = _factory.CreateClient();
+
+        var tokenService = new BusinessLogic.TokenService();
+
+        var adminUser = new Domain.Administrator
+        {
+            Id = Guid.NewGuid(),
+            Name = "Admin",
+            LastName = "User",
+            Email = "admin@example.com"
+        };
+        string adminToken = tokenService.GenerateToken(adminUser);
+        _adminClient = _factory.CreateClient();
+        _adminClient.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", adminToken);
+    }
+    
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _client?.Dispose();
+        _adminClient?.Dispose();
+        _factory?.Dispose();
+    }
+    
+    [TestMethod]
+    public async Task GetEvents_ReturnsOkResult_WithListOfEvents()
+    {
+        var mockEvents = new List<EventResponse>
+        {
+            new EventResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Event 1",
+                Date = DateTime.Now.AddDays(10),
+                Hour = 2,
+                MaxCapacity = 100,
+                CurrentCapacity = 50,
+                Cost = 100
+            },
+            new EventResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Event 2",
+                Date = DateTime.Now.AddDays(20),
+                Hour = 3,
+                MaxCapacity = 200,
+                CurrentCapacity = 150,
+                Cost = 200
+            }
+        };
+        
+        _mockEventService.Setup(service => service.GetAllEvents())
+            .ReturnsAsync(mockEvents);
+        
+        var response = await _adminClient.GetAsync("/api/events");
+        
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var eventsResponse = JsonSerializer.Deserialize<List<EventResponse>>(responseString, options);
+        
+        Assert.IsNotNull(eventsResponse);
+        Assert.AreEqual(2, eventsResponse.Count);
+        Assert.AreEqual("Event 1", eventsResponse[0].Name);
+        Assert.AreEqual("Event 2", eventsResponse[1].Name);
+    }
+}
