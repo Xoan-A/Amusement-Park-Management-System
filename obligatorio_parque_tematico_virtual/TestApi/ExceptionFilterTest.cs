14cfb3690f8c10cfa687730776a4d64a720213f0
Xoan Atrio <atrioxoan@gmail.com>
2025-10-01 14:32:48 -0300

    feat: [GREEN] created exception filter with basic exceptions handlers

---
+++ obligatorio_parque_tematico_virtual/TestApi/ExceptionFilterTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using Api.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+
+namespace ApiTests;
+
+[TestClass]
+public class ExceptionFilterTest
+{
+    private ExceptionContext _context;
+    private ExceptionFilter _attribute;
+
+    public ExceptionFilterTest()
+    {
+        _attribute = new ExceptionFilter();
+    }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _context = new ExceptionContext(
+            new ActionContext(
+                new Mock<HttpContext>(MockBehavior.Strict).Object,
+                new RouteData(),
+                new ActionDescriptor()),
+            new List<IFilterMetadata>());
+    }
+
+    [TestMethod]
+    public void OnException_WhenExceptionIsNotRegistered_ShouldResponseInternalError()
+    {
+        _context.Exception = new Exception("Not registered");
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+        var concreteResponse = response as ObjectResult;
+
+
+        Assert.AreEqual((int)HttpStatusCode.InternalServerError, concreteResponse.StatusCode);
+        Assert.AreEqual("500", concreteResponse.StatusCode.ToString());
+        Assert.AreEqual("An unexpected error occurred", GetMessage(concreteResponse.Value));
+    }
+
+    [TestMethod]
+    public void OnException_WhenExceptionIsNotImplemented_ShouldResponseNotImplemented()
+    {
+        _context.Exception = new NotImplementedException();
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+        var concreteResponse = response as ObjectResult;
+
+        Assert.AreEqual((int)HttpStatusCode.NotImplemented, concreteResponse.StatusCode);
+        Assert.AreEqual("501", concreteResponse.StatusCode.ToString());
+        Assert.AreEqual("Not implemented", GetMessage(concreteResponse.Value));
+    }
+
+    private string GetMessage(object value) =>
+        value.GetType().GetProperty("Message").GetValue(value).ToString();
+}
