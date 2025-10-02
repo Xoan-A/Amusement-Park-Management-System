e0e9a935470bd08a5f06a1f75dba3579ca4b5931
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-02 14:11:21 -0300

    feat: added RegisterEntry firm

--- obligatorio_parque_tematico_virtual/IBusinessLogic/IUserLogic.cs
+++ obligatorio_parque_tematico_virtual/IBusinessLogic/IUserLogic.cs
@@ -1,10 +1,12 @@
 using System;
 using Domain;
+using Models.In;
 
 namespace IBusinessLogic
 {
     public interface IUserLogic
     {
         Visitor RegisterVisitor(string name, string lastName, string email, string password, DateTime birthDate);
+        Task RegisterEntry(Guid userId, Guid attractionId, DateTime entrerDate);
     }
 }
