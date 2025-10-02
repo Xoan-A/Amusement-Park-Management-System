5f2c728801ee15058d9634c24128c3e5ec98936f
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-02 16:53:33 -0300

    refactor: changed tests to work with current report implementation

--- obligatorio_parque_tematico_virtual/TestDomain/ReportTests.cs
+++ obligatorio_parque_tematico_virtual/TestDomain/ReportTests.cs
@@ -18,9 +18,8 @@ public class ReportTests
         Report report = new Report(enterDate, attraction);
 
         Assert.AreEqual(enterDate, report.EnterDate);
-        Assert.IsNotNull(report.Attractions);
-        Assert.AreEqual(1, report.Attractions.Count);
-        Assert.AreEqual(attraction, report.Attractions[0]);
+        Assert.IsNotNull(report.Attraction);
+        Assert.AreEqual(attraction, report.Attraction);
     }
 
     [TestMethod]
@@ -49,7 +48,7 @@ public class ReportTests
 
         Assert.AreEqual(default(DateTime), report.ExitDate);
     }
-    
+
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void Report_ExitDate_CanNotBeSetBeforeStartDate()
@@ -61,7 +60,7 @@ public class ReportTests
 
         report.SetExitTime(exitDate);
     }
-    
+
     [TestMethod]
     public void Report_ExitDate_WhenCorrectExitDateInsertedItAddsItToReport()
     {
@@ -71,48 +70,23 @@ public class ReportTests
         Report report = new Report(enterDate, attraction);
 
         report.SetExitTime(exitDate);
-        
+
         Assert.AreEqual(exitDate, report.ExitDate);
     }
-    
+
     [TestMethod]
-    public void AddAttraction_ShouldAddAttractionToList()
+    public void Report_ShouldStoreCorrectAttraction()
     {
         DateTime enterDate = new DateTime(2025, 10, 1, 10, 0, 0);
-        Attraction firstAttraction = new Attraction
+        Attraction attraction = new Attraction
         {
             Name = "Bumper Cars",
             Type = AttractionType.InteractiveZone
         };
-        Attraction secondAttraction = new Attraction
-        {
-            Name = "Haunted House",
-            Type = AttractionType.Performance
-        };
-        Report report = new Report(enterDate, firstAttraction);
+        Report report = new Report(enterDate, attraction);
 
-        report.AddAttraction(secondAttraction);
-
-        Assert.AreEqual(2, report.Attractions.Count);
-        Assert.AreEqual(firstAttraction, report.Attractions[0]);
-        Assert.AreEqual(secondAttraction, report.Attractions[1]);
-    }
-
-    [TestMethod]
-    public void AddAttraction_ShouldAddMultipleAttractions()
-    {
-        DateTime enterDate = new DateTime(2025, 10, 1, 10, 0, 0);
-        Attraction attraction1 = new Attraction { Name = "Attraction 1" };
-        Attraction attraction2 = new Attraction { Name = "Attraction 2" };
-        Attraction attraction3 = new Attraction { Name = "Attraction 3" };
-        Report report = new Report(enterDate, attraction1);
-
-        report.AddAttraction(attraction2);
-        report.AddAttraction(attraction3);
-
-        Assert.AreEqual(3, report.Attractions.Count);
-        Assert.AreEqual(attraction1, report.Attractions[0]);
-        Assert.AreEqual(attraction2, report.Attractions[1]);
-        Assert.AreEqual(attraction3, report.Attractions[2]);
+        Assert.AreEqual(attraction, report.Attraction);
+        Assert.AreEqual("Bumper Cars", report.Attraction.Name);
+        Assert.AreEqual(AttractionType.InteractiveZone, report.Attraction.Type);
     }
 }
