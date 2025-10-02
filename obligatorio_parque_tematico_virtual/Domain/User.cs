ed17df16abad01679119ddfcfb740992f64dcb5d
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-02 13:16:43 -0300

    feat: register entry with its tests

--- obligatorio_parque_tematico_virtual/Domain/User.cs
+++ obligatorio_parque_tematico_virtual/Domain/User.cs
@@ -9,10 +9,25 @@ namespace Domain
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public List<VisitorReport> VisitorReports { get; set; }
 
         public User()
         {
             Id = Guid.NewGuid();
+        }
+
+        public void RegisterEntry(Attraction attraction, DateTime enterDate)
+        {
+            Report report = new Report(enterDate, attraction);
+            VisitorReport visitorReport = VisitorReports.FirstOrDefault(vr => vr.Date.Date == enterDate.Date);
+
+            if (visitorReport == null)
+            {
+                visitorReport = new VisitorReport(enterDate, report);
+                VisitorReports.Add(visitorReport);
+            }
+            else
+                visitorReport.AddReport(report);
         }
     }
 }
--- obligatorio_parque_tematico_virtual/TestDomain/UserTest.cs
+++ obligatorio_parque_tematico_virtual/TestDomain/UserTest.cs
@@ -104,5 +104,87 @@ namespace TestDomain
 
             Assert.AreNotEqual(user1.Id, user2.Id);
         }
+
+        [TestMethod]
+        public void RegisterEntry_ShouldCreateNewVisitorReportWhenNoneExists()
+        {
+            Visitor visitor = new Visitor
+            {
+                Name = "John",
+                LastName = "Doe",
+                VisitorReports = new List<VisitorReport>()
+            };
+            DateTime enterDate = new DateTime(2025, 10, 1, 10, 0, 0);
+            Attraction attraction = new Attraction
+            {
+                Name = "Roller Coaster",
+                Type = AttractionType.RollerCoaster
+            };
+
+            visitor.RegisterEntry(attraction, enterDate);
+
+            Assert.AreEqual(1, visitor.VisitorReports.Count);
+            Assert.AreEqual(enterDate.Date, visitor.VisitorReports[0].Date.Date);
+            Assert.AreEqual(1, visitor.VisitorReports[0].Reports.Count);
+            Assert.AreEqual(enterDate, visitor.VisitorReports[0].Reports[0].EnterDate);
+        }
+
+        [TestMethod]
+        public void RegisterEntry_ShouldAddReportToExistingVisitorReport()
+        {
+            DateTime enterDate1 = new DateTime(2025, 10, 1, 10, 0, 0);
+            DateTime enterDate2 = new DateTime(2025, 10, 1, 14, 0, 0);
+            Attraction attraction1 = new Attraction
+            {
+                Name = "Roller Coaster",
+                Type = AttractionType.RollerCoaster
+            };
+            Attraction attraction2 = new Attraction
+            {
+                Name = "Simulator",
+                Type = AttractionType.Simulator
+            };
+            Visitor visitor = new Visitor
+            {
+                Name = "John",
+                LastName = "Doe",
+                VisitorReports = new List<VisitorReport>()
+            };
+
+            visitor.RegisterEntry(attraction1, enterDate1);
+            visitor.RegisterEntry(attraction2, enterDate2);
+
+            Assert.AreEqual(1, visitor.VisitorReports.Count);
+            Assert.AreEqual(2, visitor.VisitorReports[0].Reports.Count);
+            Assert.AreEqual(enterDate1, visitor.VisitorReports[0].Reports[0].EnterDate);
+            Assert.AreEqual(enterDate2, visitor.VisitorReports[0].Reports[1].EnterDate);
+        }
+
+        [TestMethod]
+        public void RegisterEntry_ShouldCreateSeparateVisitorReportsForDifferentDays()
+        {
+            DateTime enterDate1 = new DateTime(2025, 10, 1, 10, 0, 0);
+            DateTime enterDate2 = new DateTime(2025, 10, 2, 10, 0, 0);
+            Attraction attraction = new Attraction
+            {
+                Name = "Roller Coaster",
+                Type = AttractionType.RollerCoaster
+            };
+            Visitor visitor = new Visitor
+            {
+                Name = "John",
+                LastName = "Doe",
+                VisitorReports = new List<VisitorReport>()
+            };
+
+            visitor.RegisterEntry(attraction, enterDate1);
+            visitor.RegisterEntry(attraction, enterDate2);
+
+            Assert.AreEqual(2, visitor.VisitorReports.Count);
+            Assert.AreEqual(enterDate1.Date, visitor.VisitorReports[0].Date.Date);
+            Assert.AreEqual(enterDate2.Date, visitor.VisitorReports[1].Date.Date);
+        }
+
+        
     }
 }
