3f523f94252117efa4de45c9b2826530639d3458
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2025-10-04 16:27:20 -0300

    test: [RED] add failing tests for Role repository

---
+++ obligatorio_parque_tematico_virtual/TestDataAccess/RoleRepositoryTest.cs
@@ -0,0 +1,98 @@
+using DataAccess.Context;
+using DataAccess.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestDataAccess;
+
+[TestClass]
+public class RoleRepositoryTest
+{
+    private AppDbContext _context = null!;
+    private RoleRepository _repository = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new AppDbContext(options);
+        _repository = new RoleRepository(_context);
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+    [TestMethod]
+    public void GetAll_ShouldReturnAllRoles()
+    {
+        // Arrange - Seed data should be in context
+
+        // Act
+        List<Role> roles = _repository.GetAll();
+
+        // Assert
+        Assert.IsNotNull(roles);
+        Assert.AreEqual(3, roles.Count);
+    }
+
+    [TestMethod]
+    public void GetByName_ShouldReturnRole_WhenRoleExists()
+    {
+        // Act
+        Role? role = _repository.GetByName(Role.ADMINISTRATOR);
+
+        // Assert
+        Assert.IsNotNull(role);
+        Assert.AreEqual(Role.ADMINISTRATOR, role.Name);
+    }
+
+    [TestMethod]
+    public void GetByName_ShouldReturnNull_WhenRoleDoesNotExist()
+    {
+        // Act
+        Role? role = _repository.GetByName("NonExistentRole");
+
+        // Assert
+        Assert.IsNull(role);
+    }
+
+    [TestMethod]
+    public void SeedData_ShouldContainAdministratorRole()
+    {
+        // Act
+        Role? role = _repository.GetByName(Role.ADMINISTRATOR);
+
+        // Assert
+        Assert.IsNotNull(role);
+        Assert.AreEqual("Administrator", role.Name);
+    }
+
+    [TestMethod]
+    public void SeedData_ShouldContainOperatorRole()
+    {
+        // Act
+        Role? role = _repository.GetByName(Role.OPERATOR);
+
+        // Assert
+        Assert.IsNotNull(role);
+        Assert.AreEqual("Operator", role.Name);
+    }
+
+    [TestMethod]
+    public void SeedData_ShouldContainVisitorRole()
+    {
+        // Act
+        Role? role = _repository.GetByName(Role.VISITOR);
+
+        // Assert
+        Assert.IsNotNull(role);
+        Assert.AreEqual("Visitor", role.Name);
+    }
+}
