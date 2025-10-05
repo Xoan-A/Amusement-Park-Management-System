23df4e75c428e8aa6710ef39b1d554ae00a94bfc
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-05 18:09:22 -0300

    test: perEvent tests added

--- obligatorio_parque_tematico_virtual/TestBusinessLogic/StrategyTest.cs
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/StrategyTest.cs
@@ -33,5 +33,51 @@ namespace TestBusinessLogic
 
             Assert.AreEqual("PerAttraction", strategy.Name);
         }
+        
+        [TestMethod]
+        public void PerEvent_CalculateScore_NotSpecialEvent_ShouldReturnBaseScore()
+        {
+            var strategy = new PerEvent();
+            var user = new Visitor { Name = "Test" };
+            var attraction = new Attraction { Type = AttractionType.Performance };
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction,
+                IsSepcialEvent = false
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(3, score);
+        }
+
+        [TestMethod]
+        public void PerEvent_CalculateScore_SpecialEvent_ShouldReturnDoubleScore()
+        {
+            var strategy = new PerEvent();
+            var user = new Visitor { Name = "Test" };
+            var attraction = new Attraction { Type = AttractionType.Performance };
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction,
+                IsSepcialEvent = true
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(6, score);
+        }
+
+        [TestMethod]
+        public void PerEvent_Name_ShouldBePerEvent()
+        {
+            var strategy = new PerEvent();
+
+            Assert.AreEqual("PerEvent", strategy.Name);
+        }
     }
 }
