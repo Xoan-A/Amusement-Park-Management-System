5f204563ac426bd384569c7a460be4ae54eefa0c
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2025-10-04 22:18:34 -0300

    test: [GREEN] add tests for TokenService multi-role support

--- obligatorio_parque_tematico_virtual/TestBusinessLogic/TokenServiceTest.cs
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/TokenServiceTest.cs
@@ -137,5 +137,84 @@ namespace TestBusinessLogic
 
             Assert.AreNotEqual(token1, token2);
         }
+
+        [TestMethod]
+        public void GenerateToken_ShouldIncludeAllRoles_ForUserWithMultipleRoles()
+        {
+            User user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Multi",
+                LastName = "Role",
+                Email = "multi@test.com",
+                Password = "hashedPassword"
+            };
+
+            Role adminRole = new Role { Id = 1, Name = "Administrator" };
+            Role operatorRole = new Role { Id = 2, Name = "Operator" };
+
+            user.UserRoles.Add(new UserRole { UserId = user.Id, RoleId = adminRole.Id, Role = adminRole });
+            user.UserRoles.Add(new UserRole { UserId = user.Id, RoleId = operatorRole.Id, Role = operatorRole });
+
+            string token = _tokenService.GenerateToken(user);
+
+            Assert.IsNotNull(token);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
+
+            var roleClaims = jwtToken.Claims.Where(c => c.Type == ClaimTypes.Role).ToList();
+            Assert.AreEqual(2, roleClaims.Count);
+            Assert.IsTrue(roleClaims.Any(c => c.Value == "Administrator"));
+            Assert.IsTrue(roleClaims.Any(c => c.Value == "Operator"));
+        }
+
+        [TestMethod]
+        public void GenerateToken_ShouldIncludeSingleRole_ForUserWithOneRole()
+        {
+            User user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Single",
+                LastName = "Role",
+                Email = "single@test.com",
+                Password = "hashedPassword"
+            };
+
+            Role visitorRole = new Role { Id = 3, Name = "Visitor" };
+            user.UserRoles.Add(new UserRole { UserId = user.Id, RoleId = visitorRole.Id, Role = visitorRole });
+
+            string token = _tokenService.GenerateToken(user);
+
+            Assert.IsNotNull(token);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
+
+            var roleClaims = jwtToken.Claims.Where(c => c.Type == ClaimTypes.Role).ToList();
+            Assert.AreEqual(1, roleClaims.Count);
+            Assert.AreEqual("Visitor", roleClaims[0].Value);
+        }
+
+        [TestMethod]
+        public void GenerateToken_ShouldUseClassNameFallback_ForUserWithoutRoles()
+        {
+            User user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "No",
+                LastName = "Roles",
+                Email = "noroles@test.com",
+                Password = "hashedPassword"
+            };
+
+            string token = _tokenService.GenerateToken(user);
+
+            Assert.IsNotNull(token);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
+
+            var roleClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+            Assert.IsNotNull(roleClaim);
+            Assert.AreEqual("User", roleClaim.Value);
+        }
     }
 }
