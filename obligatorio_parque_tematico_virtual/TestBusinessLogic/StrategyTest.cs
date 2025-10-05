b718cc2c0c2890d94b3c756311050390cc440c34
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-05 18:11:02 -0300

    test: combo tests added

--- obligatorio_parque_tematico_virtual/TestBusinessLogic/StrategyTest.cs
+++ obligatorio_parque_tematico_virtual/TestBusinessLogic/StrategyTest.cs
@@ -79,5 +79,199 @@ namespace TestBusinessLogic
 
             Assert.AreEqual("PerEvent", strategy.Name);
         }
+        
+        [TestMethod]
+        public void Combo_CalculateScore_FirstVisit_ShouldReturnBaseScore()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction = new Attraction { Type = AttractionType.Performance };
+
+            DateTime firstVisit = new DateTime(2025, 10, 5, 10, 0, 0);
+            user.RegisterEntry(attraction, firstVisit);
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction,
+                EnterDate = firstVisit
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(3, score);
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_SameAttraction_ShouldReturnBaseScore()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction1 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.Performance };
+
+            DateTime firstVisit = new DateTime(2025, 10, 5, 10, 0, 0);
+            user.RegisterEntry(attraction1, firstVisit);
+
+            DateTime secondVisit = new DateTime(2025, 10, 5, 10, 20, 0);
+            user.RegisterEntry(attraction1, secondVisit);
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction1,
+                EnterDate = secondVisit
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(3, score);
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_DifferentAttractionWithinTime_ShouldReturnDoubleScore()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction1 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.RollerCoaster };
+            var attraction2 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.Performance };
+
+            DateTime firstVisit = new DateTime(2025, 10, 5, 10, 0, 0);
+            user.RegisterEntry(attraction1, firstVisit);
+
+            DateTime secondVisit = new DateTime(2025, 10, 5, 10, 20, 0);
+            user.RegisterEntry(attraction2, secondVisit);
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction2,
+                EnterDate = secondVisit
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(6, score);
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_DifferentAttractionOutsideTime_ShouldReturnBaseScore()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction1 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.RollerCoaster };
+            var attraction2 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.Performance };
+
+            DateTime firstVisit = new DateTime(2025, 10, 5, 10, 0, 0);
+            user.RegisterEntry(attraction1, firstVisit);
+
+            DateTime secondVisit = new DateTime(2025, 10, 5, 10, 40, 0);
+            user.RegisterEntry(attraction2, secondVisit);
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction2,
+                EnterDate = secondVisit
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(3, score);
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_MultipleVisits_ShouldCheckMostRecent()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction1 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.RollerCoaster };
+            var attraction2 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.Simulator };
+            var attraction3 = new Attraction { Id = Guid.NewGuid(), Type = AttractionType.Performance };
+
+            DateTime firstVisit = new DateTime(2025, 10, 5, 10, 0, 0);
+            user.RegisterEntry(attraction1, firstVisit);
+
+            DateTime secondVisit = new DateTime(2025, 10, 5, 10, 25, 0);
+            user.RegisterEntry(attraction2, secondVisit);
+
+            DateTime thirdVisit = new DateTime(2025, 10, 5, 10, 40, 0);
+            user.RegisterEntry(attraction3, thirdVisit);
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction3,
+                EnterDate = thirdVisit
+            };
+
+            int score = strategy.CalculateScore(request);
+
+            Assert.AreEqual(6, score);
+        }
+
+        [TestMethod]
+        public void Combo_Name_ShouldBeCombo()
+        {
+            var strategy = new Combo(30);
+
+            Assert.AreEqual("Combo", strategy.Name);
+        }
+
+        [TestMethod]
+        public void Combo_Constructor_ShouldSetN()
+        {
+            var strategy = new Combo(45);
+
+            Assert.AreEqual(45, strategy.N);
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_ShouldThrowWhenUserIsNull()
+        {
+            var strategy = new Combo(30);
+            var attraction = new Attraction { Type = AttractionType.Performance };
+
+            var request = new StrategyRequest
+            {
+                User = null!,
+                Attraction = attraction,
+                EnterDate = new DateTime(2025, 10, 5, 10, 0, 0)
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => strategy.CalculateScore(request));
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_ShouldThrowWhenAttractionIsNull()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = null!,
+                EnterDate = new DateTime(2025, 10, 5, 10, 0, 0)
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => strategy.CalculateScore(request));
+        }
+
+        [TestMethod]
+        public void Combo_CalculateScore_ShouldThrowWhenEnterDateIsNull()
+        {
+            var strategy = new Combo(30);
+            var user = new Visitor { Name = "Test" };
+            var attraction = new Attraction { Type = AttractionType.Performance };
+
+            var request = new StrategyRequest
+            {
+                User = user,
+                Attraction = attraction,
+                EnterDate = null
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => strategy.CalculateScore(request));
+        }
     }
 }
