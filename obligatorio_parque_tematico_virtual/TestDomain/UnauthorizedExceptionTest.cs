aae60e629d582c441b4ea5a10727721b08ffc053
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2025-10-04 23:00:08 -0300

    test: [GREEN] add tests for Domain exception classes

---
+++ obligatorio_parque_tematico_virtual/TestDomain/UnauthorizedExceptionTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Exceptions;
+
+namespace TestDomain;
+
+[TestClass]
+public class UnauthorizedExceptionTest
+{
+    [TestMethod]
+    public void Constructor_WithNoArgs_SetsDefaultMessage()
+    {
+        var exception = new UnauthorizedException();
+
+        Assert.AreEqual("Unauthorized access", exception.Message);
+    }
+
+    [TestMethod]
+    public void Constructor_WithCustomMessage_SetsCustomMessage()
+    {
+        string customMessage = "Custom unauthorized message";
+        var exception = new UnauthorizedException(customMessage);
+
+        Assert.AreEqual(customMessage, exception.Message);
+    }
+}
