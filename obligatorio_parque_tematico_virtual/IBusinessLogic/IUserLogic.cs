ef30c5e56eb0c4c92aab2a8a4f3afa19234b0b94
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-04 21:55:57 -0300

    feat: implemented ticket validation on RegisterEntry

--- obligatorio_parque_tematico_virtual/IBusinessLogic/IUserLogic.cs
+++ obligatorio_parque_tematico_virtual/IBusinessLogic/IUserLogic.cs
@@ -7,7 +7,7 @@ namespace IBusinessLogic
     public interface IUserLogic
     {
         Visitor RegisterVisitor(string name, string lastName, string email, string password, DateTime birthDate);
-        Task RegisterEntry(Guid userId, Guid attractionId, DateTime entrerDate);
+        Task RegisterEntry(Guid userId, Guid attractionId, DateTime entrerDate, Guid? qr, Guid? nfc, int? eventId);
         Task RegisterExit(Guid userId, Guid attractionId, DateTime exitDate);
     }
 }
