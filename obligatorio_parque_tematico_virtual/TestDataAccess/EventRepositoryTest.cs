f5be52e7efbc9e29f711afd95bbf9e65bfeca489
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-05 18:52:44 -0300

    test: GetEventByAttractionAndDate tests added

--- obligatorio_parque_tematico_virtual/TestDataAccess/EventRepositoryTest.cs
+++ obligatorio_parque_tematico_virtual/TestDataAccess/EventRepositoryTest.cs
@@ -111,4 +111,111 @@ public class EventRepositoryTest
 
         Assert.IsNull(result);
     }
+
+    [TestMethod]
+    public async Task GetEventByAttractionAndDate_ShouldReturnEvent_WhenEventExistsWithAttractionAndDate()
+    {
+        Attraction attraction = new Attraction
+        {
+            Name = "Roller Coaster",
+            Description = "Fast ride",
+            MinAge = 12,
+            MaxCapacity = 100,
+            CurrentCapacity = 0
+        };
+
+        EventAttraction eventAttraction = new EventAttraction
+        {
+            Event = eventEntity,
+            Attraction = attraction
+        };
+
+        eventEntity.Attractions = new List<EventAttraction> { eventAttraction };
+
+        await _context.Events.AddAsync(eventEntity);
+        await _context.Attractions.AddAsync(attraction);
+        await _context.SaveChangesAsync();
+
+        Event? result = await _eventRepository.GetEventByAttractionAndDate(attraction.Id, eventEntity.Date);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(eventEntity.Id, result.Id);
+        Assert.IsTrue(result.Attractions.Any(ea => ea.AttractionId == attraction.Id));
+    }
+
+    [TestMethod]
+    public async Task GetEventByAttractionAndDate_ShouldReturnNull_WhenNoEventExistsForDate()
+    {
+        Attraction attraction = new Attraction
+        {
+            Name = "Roller Coaster",
+            Description = "Fast ride",
+            MinAge = 12,
+            MaxCapacity = 100,
+            CurrentCapacity = 0
+        };
+
+        EventAttraction eventAttraction = new EventAttraction
+        {
+            Event = eventEntity,
+            Attraction = attraction
+        };
+
+        eventEntity.Attractions = new List<EventAttraction> { eventAttraction };
+
+        await _context.Events.AddAsync(eventEntity);
+        await _context.Attractions.AddAsync(attraction);
+        await _context.SaveChangesAsync();
+
+        Event? result = await _eventRepository.GetEventByAttractionAndDate(attraction.Id, new DateTime(2024, 9, 15));
+
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public async Task GetEventByAttractionAndDate_ShouldReturnNull_WhenEventExistsButDoesNotHaveAttraction()
+    {
+        Attraction attraction1 = new Attraction
+        {
+            Name = "Roller Coaster",
+            Description = "Fast ride",
+            MinAge = 12,
+            MaxCapacity = 100,
+            CurrentCapacity = 0
+        };
+
+        Attraction attraction2 = new Attraction
+        {
+            Name = "Ferris Wheel",
+            Description = "Slow ride",
+            MinAge = 5,
+            MaxCapacity = 150,
+            CurrentCapacity = 0
+        };
+
+        EventAttraction eventAttraction = new EventAttraction
+        {
+            Event = eventEntity,
+            Attraction = attraction1
+        };
+
+        eventEntity.Attractions = new List<EventAttraction> { eventAttraction };
+
+        await _context.Events.AddAsync(eventEntity);
+        await _context.Attractions.AddAsync(attraction1);
+        await _context.Attractions.AddAsync(attraction2);
+        await _context.SaveChangesAsync();
+
+        Event? result = await _eventRepository.GetEventByAttractionAndDate(attraction2.Id, eventEntity.Date);
+
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public async Task GetEventByAttractionAndDate_ShouldReturnNull_WhenNoEventExists()
+    {
+        Event? result = await _eventRepository.GetEventByAttractionAndDate(Guid.NewGuid(), new DateTime(2024, 8, 15));
+
+        Assert.IsNull(result);
+    }
 }
