873b478b7a469ad8465fbaf147ff317fc25c9e5d
JuanMa487 <juanmanuelalvarez1807@gmail.com>
2025-10-07 19:51:01 -0300

    feat: DateTimeConfiguration and StrategyConfiguration implemented

---
+++ obligatorio_parque_tematico_virtual/Domain/StrategyConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Domain;
+
+public class StrategyConfiguration
+{
+    public int Id { get; set; }
+    public string StrategyName { get; set; } = string.Empty;
+    public int? N { get; set; }
+}
