b40b45da048078c7959438177e30dc2a9bb382f6
Xoan Atrio <atrioxoan@gmail.com>
2025-10-08 21:57:48 -0300

    fix: removed white spaces

--- obligatorio_parque_tematico_virtual/BusinessLogic/UserLogic.cs
+++ obligatorio_parque_tematico_virtual/BusinessLogic/UserLogic.cs
@@ -66,7 +66,7 @@ namespace BusinessLogic
             if (visitorRole != null)
             {
                 visitor.UserRoles.Add(new UserRole
-                    { UserId = visitor.Id, RoleId = visitorRole.Id, Role = visitorRole });
+                { UserId = visitor.Id, RoleId = visitorRole.Id, Role = visitorRole });
             }
 
             return await _userRepository.Create(visitor);
