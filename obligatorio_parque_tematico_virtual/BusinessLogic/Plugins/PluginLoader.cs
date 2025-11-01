70e2c760764d5c853b298953abc968f5ead1ee09
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2025-11-01 16:03:50 -0300

    feat: [GREEN] implement PluginLoader with reflection-based plugin discovery

---
+++ obligatorio_parque_tematico_virtual/BusinessLogic/Plugins/PluginLoader.cs
@@ -0,0 +1,145 @@
+using System.Reflection;
+using IBusinessLogic.Strategy;
+using Domain;
+
+namespace BusinessLogic.Plugins;
+
+public class PluginLoader
+{
+    private readonly string _pluginsPath;
+    private readonly Dictionary<string, PluginInfo> _availablePlugins;
+
+    public PluginLoader(string pluginsPath)
+    {
+        _pluginsPath = pluginsPath;
+        _availablePlugins = new Dictionary<string, PluginInfo>();
+        LoadPlugins();
+    }
+
+    public List<PluginInfo> LoadPlugins()
+    {
+        _availablePlugins.Clear();
+
+        if (!Directory.Exists(_pluginsPath))
+        {
+            return new List<PluginInfo>();
+        }
+
+        string[] dllFiles = Directory.GetFiles(_pluginsPath, "*.dll");
+
+        foreach (string dllFile in dllFiles)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(dllFile);
+                DiscoverPlugins(assembly, dllFile);
+            }
+            catch (Exception)
+            {
+                // Skip assemblies that can't be loaded
+                continue;
+            }
+        }
+
+        return _availablePlugins.Values.ToList();
+    }
+
+    private void DiscoverPlugins(Assembly assembly, string assemblyPath)
+    {
+        Type[] types;
+        try
+        {
+            types = assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException)
+        {
+            return;
+        }
+
+        foreach (Type type in types)
+        {
+            if (type.IsClass && !type.IsAbstract && typeof(IConcreteStrategy).IsAssignableFrom(type))
+            {
+                try
+                {
+                    // Try to create an instance to get the Name property
+                    IConcreteStrategy? instance = Activator.CreateInstance(type) as IConcreteStrategy;
+                    if (instance != null)
+                    {
+                        PluginInfo pluginInfo = new PluginInfo
+                        {
+                            Name = instance.Name,
+                            Description = type.GetCustomAttribute<PluginDescriptionAttribute>()?.Description ?? string.Empty,
+                            Author = type.GetCustomAttribute<PluginAuthorAttribute>()?.Author ?? string.Empty,
+                            Version = assembly.GetName().Version?.ToString() ?? "1.0.0",
+                            AssemblyPath = assemblyPath,
+                            TypeName = type.FullName ?? type.Name
+                        };
+
+                        _availablePlugins[instance.Name] = pluginInfo;
+                    }
+                }
+                catch (Exception)
+                {
+                    // Skip strategies that can't be instantiated with default constructor
+                    continue;
+                }
+            }
+        }
+    }
+
+    public PluginInfo? GetPluginByName(string name)
+    {
+        return _availablePlugins.GetValueOrDefault(name);
+    }
+
+    public IConcreteStrategy CreateStrategyInstance(string name)
+    {
+        if (!_availablePlugins.TryGetValue(name, out PluginInfo? pluginInfo))
+        {
+            throw new KeyNotFoundException($"Plugin '{name}' not found");
+        }
+
+        Assembly assembly = Assembly.LoadFrom(pluginInfo.AssemblyPath);
+        Type? type = assembly.GetType(pluginInfo.TypeName);
+
+        if (type == null)
+        {
+            throw new InvalidOperationException($"Type '{pluginInfo.TypeName}' not found in assembly");
+        }
+
+        object? instance = Activator.CreateInstance(type);
+        if (instance is not IConcreteStrategy strategy)
+        {
+            throw new InvalidOperationException($"Type '{pluginInfo.TypeName}' does not implement IConcreteStrategy");
+        }
+
+        return strategy;
+    }
+
+    public List<string> GetAvailablePluginNames()
+    {
+        return _availablePlugins.Keys.ToList();
+    }
+}
+
+// Custom attributes for plugin metadata
+[AttributeUsage(AttributeTargets.Class)]
+public class PluginDescriptionAttribute : Attribute
+{
+    public string Description { get; }
+    public PluginDescriptionAttribute(string description)
+    {
+        Description = description;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class PluginAuthorAttribute : Attribute
+{
+    public string Author { get; }
+    public PluginAuthorAttribute(string author)
+    {
+        Author = author;
+    }
+}
