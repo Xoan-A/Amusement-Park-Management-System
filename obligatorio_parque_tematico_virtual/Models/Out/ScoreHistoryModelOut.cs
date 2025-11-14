794a372461f1d70efa1cd14656d37d02238ac3dd
Xoan Atrio <atrioxoan@gmail.com>
2025-11-14 11:16:57 -0300

    fix: removed description from ScoreHistoryModelOut

--- obligatorio_parque_tematico_virtual/Models/Out/ScoreHistoryModelOut.cs
+++ obligatorio_parque_tematico_virtual/Models/Out/ScoreHistoryModelOut.cs
@@ -7,7 +7,6 @@ public class ScoreHistoryModelOut
     public string? VisitorName { get; set; }
     public int Points { get; set; }
     public string Origin { get; set; }
-    public string Description { get; set; }
     public string StrategyName { get; set; }
     public Guid? RelatedEntityId { get; set; }
     public DateTime CreatedAt { get; set; }
